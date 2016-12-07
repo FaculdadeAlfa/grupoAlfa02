af2c84cac4ec185b03c49099df81d12528762274
Fábio <fabio.dev.gomes@gmail.com>
2016-12-07 12:37:21 -0200

    Teste autor do commit - email configurado

--- src/com/fabio/util/JPAUtil.java
+++ src/com/fabio/util/JPAUtil.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.hibernate.stat.Statistics;
 
 public class JPAUtil {
 	
