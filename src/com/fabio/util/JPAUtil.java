8efb2fea98ac9f2b6505e311722267e0a3925065
fabio <fabio@fabio-notebook>
2016-12-07 10:33:33 -0200

    Ajuste para eliminar erro de import de biblioteca

--- src/com/fabio/util/JPAUtil.java
+++ src/com/fabio/util/JPAUtil.java
@@ -4,8 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.stat.Statistics;
 
 public class JPAUtil {
@@ -16,20 +14,20 @@ public class JPAUtil {
 		return ENTITY_MANAGER_FACTORY.createEntityManager();
 	}
 	
-	public static Statistics getStatistics(){
-		EntityManager em = JPAUtil.getEntityManager();
-		Session session = (Session) em.getDelegate();
-		SessionFactory factory = session.getSessionFactory();
-		Statistics statistics = factory.getStatistics();
-		
-		return statistics;
-	}
+//	public static Statistics getStatistics(){
+//		EntityManager em = JPAUtil.getEntityManager();
+//		Session session = (Session) em.getDelegate();
+//		SessionFactory factory = session.getSessionFactory();
+//		Statistics statistics = factory.getStatistics();
+//		
+//		return statistics;
+//	}
 
-	public static void printStatistics(){
-		Statistics statistics = getStatistics();
-		System.out.println("Quantidade de entidades buscadas: "+ statistics.getEntityFetchCount());
-		System.out.println("Quantidade de entidades Carregadas: "+ statistics.getEntityLoadCount());
-		System.out.println("Quantidade de listas buscadas: "+ statistics.getCollectionFetchCount());
-	}
+//	public static void printStatistics(){
+//		Statistics statistics = getStatistics();
+//		System.out.println("Quantidade de entidades buscadas: "+ statistics.getEntityFetchCount());
+//		System.out.println("Quantidade de entidades Carregadas: "+ statistics.getEntityLoadCount());
+//		System.out.println("Quantidade de listas buscadas: "+ statistics.getCollectionFetchCount());
+//	}
 
 }
